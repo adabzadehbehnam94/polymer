c6c68ba0668ff7a90d42c32b4c135ea58d2ec078
behnam <adabzadehbehnam94>
2026-09-03 00:16:50 +0330

    fix ui title slider and change favicon

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -9,7 +9,7 @@ export default function Services(props :ServicesProps){
     
     const Icon = props.image
     return(
-        <div  className="flex flex-col py-5  items-center justify-center gap-2 opacity-60 rounded-lg bg-black text-white lg:h-50">
+        <div  className="flex flex-col py-5  items-center justify-center gap-2  rounded-lg bg-black/55   backdrop-blur-[3px] text-white lg:h-50">
             <Icon className="mx-auto lg:h-20 lg:w-20" size={50} color= "#79b72d"/>
             <h3 className="lg:text-2xl">{props.title}</h3>
         </div>
--- app/components/slider.tsx
+++ app/components/slider.tsx
@@ -39,11 +39,11 @@ export default function Slider({ sliderData }: { sliderData: sliderType[] }) {
                             className="rounded-lg text-white flex flex-col md:flex-row justify-center items-center h-80  md:h-100 overflow-hidden bg-center bg-cover bg-no-repeat "
                             style={{ backgroundImage: `url(${item.background})` }}
                         >
-                            <div className=" md:w-100 order-last md:order-first mx-auto ">
-                                <h1 className="md:text-3xl md:mb-5 mb-3 text-center md:text-start">{item.title}</h1>
-                                {item?.subtitle && <p className="md:text-sm text-[#b9bab8] text-xs md:mb-5 mb-3 text-center md:text-start">{item.subtitle}</p>}
+                            <div className=" md:w-100 order-last md:order-first mx-auto bg-black/45 backdrop-blur-[3px] rounded-xl p-5">
+                                <h1 className="md:text-3xl md:mb-5 mb-3 text-center md:text-start text-shadow-lg/30 ">{item.title}</h1>
+                                {item?.subtitle && <p className="md:text-sm text-[#b9bab8] text-xs md:mb-5 mb-3 text-center md:text-start text-shadow-lg/30">{item.subtitle}</p>}
                                 <div className='flex justify-center md:justify-start'>
-                                    <button onClick={() => router.push("/pages/products")} className="ml-5 bg-black md:px-5 md:py-3 text-[#b9bab8] rounded-md border-2 border-[#b9bab8] text-sm hover:bg-[#3b9821] hover:border-[#3b9821] hover:text-white transition duration-300 delay-100 cursor-pointer px-2 py-1">مشاهده محصولات</button>
+                                    <button onClick={() => router.push("/pages/products")} className="ml-5 bg-black md:px-5 md:py-3 text-[#b9bab8] rounded-md border-2 border-[#b9bab8] text-sm hover:bg-[#3b9821] hover:border-[#3b9821] hover:text-white transition duration-300 delay-100 cursor-pointer px-2 py-1 ">مشاهده محصولات</button>
                                     <button onClick={() => router.push("/pages/contactUs")} className=" md:px-5 bg-black md:py-3 rounded-md text-[#b9bab8] border-2 border-[#b9bab8] text-sm hover:bg-[#3b9821] hover:border-[#3b9821] hover:text-white transition duration-300 delay-100 cursor-pointer px-2 py-1">تماس با ما</button>
                                 </div>
                             </div>
